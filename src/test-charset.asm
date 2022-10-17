3d95d3ee24f93c64dd8e5546a586fe1045eb2ebc
Jeroen Bakker <j.bakker@atmind.nl>
2022-10-17 13:14:24 +0200

    Added some tests.

---
+++ src/test-charset.asm
@@ -0,0 +1,19 @@
+    processor 6502
+    org 2048
+
+    include "defines.asm"
+    include "basic_header.asm"
+
+start:
+	/* text-mem at 0400, font-mem at 2000 */
+	lda #$18
+	sta $d018
+
+end:
+	rts
+
+	org    $1ffe
+	incbin "ahoy_art_deco.64c"
+
+
+
--- src/test-sprite-duplication.asm
+++ src/test-sprite-duplication.asm
@@ -1,12 +1,21 @@
-    processor 6502
-    org 2048
+  processor 6502
+  org 2048
 
-    include "defines.asm"
-
-    include "basic_header.asm"
+  include "defines.asm"
+  include "basic_header.asm"
 
 
 start:
+
+    lda #1
+    sta SPRITE_ENABLE
+    lda #(sprite1 / 64)
+    sta SPRITE_0_NUM
+
+    lda #COLOR_BLACK
+    sta SPRITE_0_COLOR
+
+setup_irqs:
     SEI                  ; set interrupt bit, make the CPU ignore interrupt requests
     LDA #%01111111       ; switch off interrupt signals from CIA-1
     STA $DC0D
@@ -22,7 +31,7 @@ start:
     LDA #>irq
     STA $0315
 
-    LDA #0             ; set rasterline where interrupt shall occur
+    LDA #50             ; set rasterline where interrupt shall occur
     STA $D012
 
     LDA #%00000001       ; enable raster interrupt signals from VIC
@@ -32,38 +41,43 @@ start:
 loop:
     jmp loop
 
-; IRQ to draw black background + border
 irq:
-    lda #COLOR_BLACK
-    sta BACKGROUND_COLOR
-    sta BORDER_COLOR
+    lda #50
+    sta SPRITE_0_X
+    lda #50
+    sta SPRITE_0_Y
 
     LDA #<irq2            ; set interrupt vectors to the second interrupt service routine at Irq2
     STA $0314
     LDA #>irq2
     STA $0315
 
-    LDA #200
+    LDA #71
     STA $D012            ; next interrupt will occur at line no. 200
 
     ASL $D019            ; acknowledge the interrupt by clearing the VIC's interrupt flag
 
     JMP $EA81            ; jump into shorter ROM routine to only restore registers from the stack etc
 
-; IRQ to draw grey background + border
 irq2:
-    lda #COLOR_GREY
-    sta BACKGROUND_COLOR
-    sta BORDER_COLOR
+    lda #50
+    sta SPRITE_0_X
+    lda #71
+    sta SPRITE_0_Y
 
-    LDA #<irq             ; set interrupt vectors back to the first interrupt service routine at Irq
+    LDA #<irq            ; set interrupt vectors to the second interrupt service routine at Irq2
     STA $0314
     LDA #>irq
     STA $0315
 
-    LDA #202
-    STA $D012            ; next interrupt will occur at line no. 202
+    LDA #50
+    STA $D012            ; next interrupt will occur at line no. 200
 
     ASL $D019            ; acknowledge the interrupt by clearing the VIC's interrupt flag
 
     JMP $EA81            ; jump into shorter ROM routine to only restore registers from the stack etc
+
+
+    align 64
+sprite1:
+    include "sprite-data-1.asm"
