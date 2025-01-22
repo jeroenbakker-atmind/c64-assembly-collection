67ef7c74df673ed2d546edf98397733e61e6cd39
Jeroen Bakker <jeroen@blender.org>
2025-01-22 10:49:20 +0100

    Use instruction def in dasm to extract name of instruction

--- src/c64-assembler/src/generator/dasm.rs
+++ src/c64-assembler/src/generator/dasm.rs
@@ -85,21 +85,9 @@ impl DasmGenerator {
             } else {
                 line.push("  ".to_string());
             }
-            match &instruction.operation {
-                Operation::ADC => line.push("adc".to_string()),
-                Operation::AND => line.push("and".to_string()),
-                Operation::ASL => line.push("asl".to_string()),
-                Operation::BCC => line.push("bcc".to_string()),
-                Operation::BCS => line.push("bcs".to_string()),
-                Operation::LDA => line.push("lda".to_string()),
-                Operation::LDY => line.push("ldy".to_string()),
-                Operation::STA => line.push("sta".to_string()),
-                Operation::JMP => line.push("jmp".to_string()),
-                Operation::JSR => line.push("jsr".to_string()),
-                Operation::SEC => line.push("sec".to_string()),
-                Operation::CLC => line.push("clc".to_string()),
-                Operation::RTS => line.push("rts".to_string()),
 
+            // Add operation
+            match &instruction.operation {
                 Operation::Raw(bytes) => {
                     line.push(format!("byte ${:02X}", bytes[0]));
                     for i in 1..bytes.len() {
@@ -107,51 +95,12 @@ impl DasmGenerator {
                     }
                 }
                 Operation::Label(label) => line.push(format!("{}:", label)),
-                Operation::BEQ => line.push("beq".to_string()),
-                Operation::BIT => line.push("bit".to_string()),
-                Operation::BMI => line.push("bmi".to_string()),
-                Operation::BNE => line.push("bne".to_string()),
-                Operation::BPL => line.push("bpl".to_string()),
-                Operation::BRK => line.push("brk".to_string()),
-                Operation::BVC => line.push("bvc".to_string()),
-                Operation::BVS => line.push("bvs".to_string()),
-                Operation::CLD => line.push("cld".to_string()),
-                Operation::CLI => line.push("cli".to_string()),
-                Operation::CLV => line.push("clv".to_string()),
-                Operation::CMP => line.push("cmp".to_string()),
-                Operation::CPX => line.push("cpx".to_string()),
-                Operation::CPY => line.push("cpy".to_string()),
-                Operation::DEC => line.push("dec".to_string()),
-                Operation::DEX => line.push("dex".to_string()),
-                Operation::DEY => line.push("dey".to_string()),
-                Operation::EOR => line.push("eor".to_string()),
-                Operation::INC => line.push("inc".to_string()),
-                Operation::INX => line.push("inx".to_string()),
-                Operation::INY => line.push("iny".to_string()),
-                Operation::LDX => line.push("ldx".to_string()),
-                Operation::LSR => line.push("lsr".to_string()),
-                Operation::NOP => line.push("nop".to_string()),
-                Operation::ORA => line.push("ora".to_string()),
-                Operation::PHA => line.push("pha".to_string()),
-                Operation::PHP => line.push("php".to_string()),
-                Operation::PLA => line.push("pla".to_string()),
-                Operation::PLP => line.push("plp".to_string()),
-                Operation::ROL => line.push("rol".to_string()),
-                Operation::ROR => line.push("ror".to_string()),
-                Operation::RTI => line.push("rti".to_string()),
-                Operation::SBC => line.push("sbc".to_string()),
-                Operation::SED => line.push("sed".to_string()),
-                Operation::SEI => line.push("sei".to_string()),
-                Operation::STX => line.push("stx".to_string()),
-                Operation::STY => line.push("sty".to_string()),
-                Operation::TAX => line.push("tax".to_string()),
-                Operation::TAY => line.push("tay".to_string()),
-                Operation::TSX => line.push("tsx".to_string()),
-                Operation::TXA => line.push("txa".to_string()),
-                Operation::TXS => line.push("txs".to_string()),
-                Operation::TYA => line.push("tya".to_string()),
+                _ => {
+                    line.push(instruction.operation.definition().unwrap().instruction.to_string());
+                }
             }
 
+            // Add address mode
             match &instruction.address_mode {
                 AddressMode::Implied => {}
                 AddressMode::Immediate(immediate) => match immediate {
