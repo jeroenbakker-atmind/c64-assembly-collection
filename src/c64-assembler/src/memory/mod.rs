4de64af8a3a3f052be685d5616da55d86682bccd
Jeroen Bakker <jeroen@blender.org>
2025-01-22 10:24:44 +0100

    Cleanup use &self in traits

--- src/c64-assembler/src/memory/mod.rs
+++ src/c64-assembler/src/memory/mod.rs
@@ -6,21 +6,21 @@ pub mod user_count;
 pub type Address = u16;
 
 pub trait ZeroPage {
-    fn is_zeropage(self) -> bool;
-    fn low(self) -> u8;
-    fn high(self) -> u8;
+    fn is_zeropage(&self) -> bool;
+    fn low(&self) -> u8;
+    fn high(&self) -> u8;
 }
 
 impl ZeroPage for Address {
-    fn is_zeropage(self) -> bool {
-        self < 0x100
+    fn is_zeropage(&self) -> bool {
+        *self < 0x100
     }
 
-    fn low(self) -> u8 {
+    fn low(&self) -> u8 {
         (self & 0xFF) as u8
     }
 
-    fn high(self) -> u8 {
+    fn high(&self) -> u8 {
         (self >> 8) as u8
     }
 }
