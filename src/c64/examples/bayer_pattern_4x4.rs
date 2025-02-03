9c7c7ac9b176ff1aebe9197c90c0f69844c2e32e
Jeroen Bakker <jeroen@blender.org>
2025-02-03 14:08:47 +0100

    Make larger bayer patterns

--- src/c64/examples/bayer_pattern_4x4.rs
+++ src/c64/examples/bayer_pattern_4x4.rs
@@ -2,11 +2,14 @@ use c64::{image_container::SRGBImageContainer, image_io::write_png::write_png};
 use c64_colors::colors::SRGB;
 use c64_dithering::dithering::bayer::{Bayer4x4, DitherPattern};
 
+const WIDTH: usize = 1920;
+const HEIGHT:usize = 1080;
+
 fn main() {
     let pattern = Bayer4x4 {};
     let mut colors = vec![];
-    for y in 0..100 {
-        for x in 0..160 {
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
             let threshold = pattern.threshold(x, y);
             colors.push(SRGB {
                 r: threshold,
@@ -16,8 +19,8 @@ fn main() {
         }
     }
     let bayer_image = SRGBImageContainer {
-        width: 160,
-        height: 100,
+        width: WIDTH,
+        height: HEIGHT,
         buffer: colors,
     };
     write_png("resources/bayer_4x4.png", &bayer_image);
